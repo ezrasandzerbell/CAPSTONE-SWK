1c09c7e05be86635adf76cd2012f633933a9b9bc
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2017-04-25 16:59:53 -0700

    fix deprecated config to config.public_file_server.enabled = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
   config.assets.compile = true
 
   # Disable serving static files from the `/public` folder by default since
