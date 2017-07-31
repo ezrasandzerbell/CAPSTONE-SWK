77a24dc0e6a64145ad1e390adcb5cdc1e26c036c
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-07-31 00:10:27 -0700

    update styling on riffs page

--- app/assets/javascripts/songforms.js
+++ app/assets/javascripts/songforms.js
@@ -1,50 +1,50 @@
 $(document).on ("turbolinks:load", function(){
   event.preventDefault()
 
-  // Show and hide riff form
+  // Show and hide image form
   $("#addImage").click(function(){
     $("#riff_image_form").show();
-    $("#riff_thoughts_form").hide();
-    $("#sheet_music_form").hide();
-    $("#song_audio_form").hide();
+    $("#addImage").hide();
   });
-  $("#hide_riff_form").click(function(){
+
+  $("#hide_image_form").click(function(){
     $("#riff_image_form").hide();
+    $("#addImage").show();
   });
+
 
   // Show and hide thoughts form
-
   $("#addThoughts").click(function(){
     $("#riff_thoughts_form").show();
-    $("#riff_image_form").hide();
-    $("#sheet_music_form").hide();
-    $("#song_audio_form").hide();
+    $("#addThoughts").hide();
   });
-  $("#hide_thoughts_form").click(function(){
+
+  $("#hide_thoughts_form").click(function(event){
+    event.preventDefault();
     $("#riff_thoughts_form").hide();
+    $("#addThoughts").show();
   });
+
 
   // Show and hide Sheet Music form
-
   $("#addSheetMusic").click(function(){
     $("#sheet_music_form").show();
-    $("#riff_image_form").hide();
-    $("#riff_thoughts_form").hide();
-    $("#song_audio_form").hide();
+    $("#addSheetMusic").hide()
+    $("#createSheetMusic").hide();
   });
   $("#hide_sheet_music_form").click(function(){
     $("#sheet_music_form").hide();
+    $("#addSheetMusic").show();
+    $("#createSheetMusic").show();
   });
 
   // Show and hide Audio form
   $("#addSongAudio").click(function(){
     $("#song_audio_form").show();
-    $("#riff_image_form").hide();
-    $("#riff_thoughts_form").hide();
-    $("#sheet_music_form").hide();
+    $('#addSongAudio').hide();
   });
-  $("#hide_sheet_music_form").click(function(){
+  $("#hide_audio_form").click(function(){
     $("#song_audio_form").hide();
+    $('#addSongAudio').show();
   });
-
 });
